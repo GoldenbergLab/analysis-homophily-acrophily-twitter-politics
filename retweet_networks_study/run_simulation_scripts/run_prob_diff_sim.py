8a7a24f62e0a25237f9e38ef767543283ac9ee31
ddbailey92 <ddbailey@ucla.edu>
2022-05-05 13:55:56 -0700

    update script to run probability difference simulation

--- retweet_networks_study/run_simulation_scripts/run_prob_diff_sim.py
+++ retweet_networks_study/run_simulation_scripts/run_prob_diff_sim.py
@@ -1,36 +1,13 @@
-"""
-This script imports the 'run_sim' function from the simulation_modules directory
-to run the probability difference simulation on a select fraction of either liberal
-or conservative users in the dataset.
-
-Arguments:
-    orient: string, 'left' or 'right'
-        Subsets users by political orientation
-    frac_data: boolean, True or False
-        Denotes whether to run simulation on fraction of users in dataset.
-    frac_start: float, [0.0, 1.0)
-        Decides on starting fraction of users if frac_data set to True.
-    frac_end: float, (0.0, 1.0]
-        Decides on end fraction of users if frac_data set to True.
-"""
-
-# Append path to import functions from prob diff sim module:
+import os
 import sys
-sys.path.append('../simulation_functions/')
+
+appended_path = os.path.join('..', 'simulation_modules')
+sys.path.append(appended_path)
 
 # Import functions from prob diff sim module:
-from prob_diff_sim_functions import run_sim
+from sims_module import ProbDiffSim
 
-# Run for first 10% of liberal users in dataset:
 if __name__ == '__main__':
-
-    # Defining political orientation of users (left or right):
-    orient = 'left'
-
-    # Defining fraction of users to include in simulation:
-    frac_data = True
-    frac_start = 0.0
-    frac_end = 0.03
-
-    # Running simulation:
-    run_sim(orient, frac_data=frac_data, frac_start=frac_start, frac_end=frac_end)
+    prob_diff_sim = ProbDiffSim(orient='right', frac_data=True,
+                                frac_start=0.0, frac_end=0.01)
+    prob_diff_sim.run()
