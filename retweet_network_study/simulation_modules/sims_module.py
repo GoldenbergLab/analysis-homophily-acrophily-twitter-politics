95fbcb1f8f02da208cef0a6af6d43dd439765b35
ddbailey92 <ddbailey@ucla.edu>
2022-05-06 01:37:58 -0700

    add mean abs diff simulation class

--- retweet_network_study/simulation_modules/sims_module.py
+++ retweet_network_study/simulation_modules/sims_module.py
@@ -1,24 +1,27 @@
-import os
-
-import pandas as pd
-pd.options.mode.chained_assignment = None
-
-import numpy as np
-
-import time
-
 from sims_data_prep import TwitterDataProcessor
+import os
+import numpy as np
+import time
+import pandas as pd
+
+pd.options.mode.chained_assignment = None
 
 
 # Create probability difference simulation class:
 class ProbDiffSim(TwitterDataProcessor):
 
-    
     # Inherit processed data from data prep class:
     def __init__(self, orient, frac_data=False, frac_start=None, frac_end=None):
         TwitterDataProcessor.__init__(self, orient, frac_data, frac_start, frac_end)
         self.rt_df = self.get_retweet_data()
 
+        # Vectorized lambda function to count whether peer rating is greater than ego rating:
+        self.count_more_extreme = np.vectorize(lambda x, y: 1 if y > x else 0)
+
+        # Initializing dataframes that will be created during run of simulation:
+        self.homophily_df = None
+        self.sim_df = None
+        self.prob_diff_df = None
 
     # Run homophily simulation and append homophily peer ratings to new column in dataframe:
     def get_homophily_df(self):
@@ -34,11 +37,11 @@ class ProbDiffSim(TwitterDataProcessor):
         closest_peers = []
 
         # Take 70% subset of ego ratings:
-        ego_ratings_subset = ego_ratings[0:int(0.7*len(ego_ratings))]
+        n_subset = int(0.7*len(ego_ratings))
+        ego_ratings_subset = ego_ratings[0:n_subset]
 
         # For each ego rating in subset:
         for ego_rating in ego_ratings_subset:
-
             # Find absolute differences:
             abs_diffs = np.abs(peer_ratings - ego_rating)
 
@@ -60,12 +63,8 @@ class ProbDiffSim(TwitterDataProcessor):
         # Create new column for closest peers based on homophily strategy:
         self.homophily_df['homoph_rating_peer'] = closest_peers
 
-
     # Creates dataframe with user id and probability differences after n trials:
     def get_sim_df(self):
-
-        # Vectorized lambda function to count whether peer rating is greater than ego rating:
-        count_more_extreme = np.vectorize(lambda x, y: 1 if y > x else 0)
 
         # Initialize dataframe:
         self.sim_df = pd.DataFrame()
@@ -76,8 +75,8 @@ class ProbDiffSim(TwitterDataProcessor):
         elif self.orient == 'left':
             print('Beginning liberal group simulation.', flush=True)
 
-        if self.frac_data == True:
-            print(f'Fractions chosen: {int(self.frac_start*100)}% to {int(self.frac_end*100)}%.', flush=True)
+        if self.frac_data is True:
+            print(f'Fractions chosen: {int(self.frac_start * 100)}% to {int(self.frac_end * 100)}%.', flush=True)
 
         # Run for 100 trials and continually add to the more_extreme_count dataframe:
         for i in range(100):
@@ -96,14 +95,15 @@ class ProbDiffSim(TwitterDataProcessor):
             peer_ratings_homoph = self.homophily_df['homoph_rating_peer']
 
             # Create count column indicating whether peer is more extreme for each condition:
-            self.homophily_df['is_more_extreme_homoph'] = count_more_extreme(ego_ratings, peer_ratings_homoph)
-            self.homophily_df['is_more_extreme_empi'] = count_more_extreme(ego_ratings, peer_ratings_empi)
+            self.homophily_df['is_more_extreme_homoph'] = self.count_more_extreme(ego_ratings, peer_ratings_homoph)
+            self.homophily_df['is_more_extreme_empi'] = self.count_more_extreme(ego_ratings, peer_ratings_empi)
 
             # Append results to dataframe:
             self.sim_df = pd.concat([self.sim_df, self.homophily_df], axis=0, ignore_index=True)
 
             # Track trial runtime:
-            print(f'Current iteration complete. Time elapsed: {(time.time() - start_time) / 60: .2f} minutes.', '\n', flush=True)
+            minutes_taken = (time.time() - start_time) / 60
+            print(f'Current iteration complete. Time elapsed: {minutes_taken: .2f} minutes.', '\n', flush=True)
 
         print('Simulation complete. Creating dataframe.', flush=True)
 
@@ -127,15 +127,17 @@ class ProbDiffSim(TwitterDataProcessor):
         data_path = os.path.join('..', 'data')
 
         if self.orient == 'right':
-            path_beginning = os.path.join(data_path, 'prob_diff_cons')
+            path_beginning = os.path.join(data_path, 'prob_diff_left')
 
         elif self.orient == 'left':
-            path_beginning = os.path.join(data_path, 'prob_diff_libs')
+            path_beginning = os.path.join(data_path, 'prob_diff_right')
+        else:
+            raise Exception('Political orientation must be defined as left or right.')
 
         if self.frac_data is True:
-            file_path = path_beginning + '_' + str(self.frac_start) + '_' + str(self.frac_end) + '.csv'
+            file_path = path_beginning + f'_{self.frac_start}_{self.frac_end}.csv'
         else:
-            file_path = path_beginning+'.csv'
+            file_path = f'{path_beginning}.csv'
 
         self.prob_diff_df.to_csv(file_path, index=False)
         print('Dataframe saved.', flush=True)
@@ -144,3 +146,100 @@ class ProbDiffSim(TwitterDataProcessor):
         self.get_sim_df()
         self.get_prob_diff_df()
         self.save_prob_diff_df()
+
+
+class MeanAbsDiffSim(TwitterDataProcessor):
+
+    # Inherit processed data from data prep class:
+    def __init__(self, orient, thresholds=range(5, 45, 5), frac_data=False, frac_start=None, frac_end=None):
+        TwitterDataProcessor.__init__(self, orient, frac_data, frac_start, frac_end)
+        self.rt_df = self.get_retweet_data()
+        self.thresholds = thresholds
+
+        self.rt_df_subset = None
+        self.mean_abs_diff_df = None
+        self.threshold_df = None
+        self.sim_df = None
+
+    def get_random_users(self, fraction=0.7):
+
+        users = self.rt_df['userid'].unique()
+        n_sample_users = int(fraction*len(users))
+        users_subset = np.random.choice(users, size=n_sample_users, replace=False)
+
+        self.rt_df_subset = self.rt_df[self.rt_df['userid'].isin(users_subset)]
+
+    def get_abs_diffs(self):
+
+        ego_ratings = self.rt_df_subset['orig_rating_ego'].values
+        peer_ratings_empi = self.rt_df_subset['orig_rating_peer'].values
+        peer_ratings_random = np.random.permutation(peer_ratings_empi)
+
+        self.rt_df_subset['abs_diff_empi'] = np.abs(ego_ratings - peer_ratings_empi)
+        self.rt_df_subset['abs_diff_random'] = np.abs(ego_ratings - peer_ratings_random)
+
+        return self.rt_df_subset
+
+    def get_sim_df(self):
+
+        if self.orient == 'left':
+            print('Beginning liberal simulation.', flush=True)
+
+        elif self.orient == 'right':
+            print('Beginning conservative simulation.', flush=True)
+
+        else:
+            raise Exception("Political orientation must be defined as left or right.")
+
+        self.sim_df = pd.DataFrame()
+        for threshold in self.thresholds:
+
+            self.rt_df = self.rt_df[self.rt_df['rt'] >= threshold]
+
+            print(f'Current threshold: {threshold} of {self.thresholds[-1]}', flush=True)
+
+            self.threshold_df = pd.DataFrame()
+
+            for i in range(100):
+
+                self.get_random_users()
+                abs_diff_df = self.get_abs_diffs()
+
+                threshold_df = pd.concat([self.threshold_df, abs_diff_df], axis=0, ignore_index=True)
+
+            threshold_df = threshold_df.groupby('userid', as_index=False)\
+                .agg(mean_abs_diff_empi=('abs_diff_empi', 'mean'),
+                     mean_abs_diff_random=('abs_diff_random', 'mean'))
+
+            threshold_df['threshold'] = np.repeat(threshold, len(threshold_df))
+
+            self.sim_df = pd.concat([self.sim_df, threshold_df], axis=0, ignore_index=True)
+
+        print('Simulation complete. Taking average results by threshold.', flush=True)
+        self.sim_df = self.sim_df.groupby('threshold', as_index=False).agg(
+            mean_abs_diff_empi=('mean_abs_diff_empi', 'mean'),
+            mean_abs_diff_random=('mean_abs_diff_random', 'mean'))
+
+        self.sim_df['poli_affil'] = np.repeat(self.orient, len(self.sim_df))
+
+    def save_sim_df(self):
+
+        print('Average results taken. Saving final dataframe.', flush=True)
+        data_path = os.path.join('..', 'data')
+
+        if self.orient == 'left':
+            path_beginning = os.path.join(data_path, 'mean_abs_diff_left')
+
+        elif self.orient == 'right':
+            path_beginning = os.path.join(data_path, 'mean_abs_diff_right')
+        else:
+            raise Exception('Political orientation must be defined as left or right.')
+
+        file_path = path_beginning + f'_{self.thresholds[0]}_{self.thresholds[-1]}.csv'
+
+        self.sim_df.to_csv(file_path, index=False)
+        print('Dataframe saved.', flush=True)
+
+    def run(self):
+        self.get_sim_df()
+        self.save_sim_df()
