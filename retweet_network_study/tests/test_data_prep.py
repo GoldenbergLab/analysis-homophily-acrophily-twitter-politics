6e2ac75ae77e70f3f40d862fe1d2b89de0940164
ddbailey92 <ddbailey@ucla.edu>
2022-05-08 01:21:55 -0700

    add tests directory with tests for twitter data prep module

---
+++ retweet_network_study/tests/test_data_prep.py
@@ -0,0 +1,84 @@
+import os
+import sys
+import unittest
+import numpy as np
+
+appended_path = os.path.join('..', 'simulation_modules')
+sys.path.append(appended_path)
+
+from sims_data_prep import TwitterDataProcessor, SimDataProcessor
+
+class TestTwitterDataProcessor(unittest.TestCase):
+
+    @classmethod
+    def setUp(cls):
+        print('setupClass')
+
+    @classmethod
+    def tearDown(cls):
+        print('teardownClass')
+
+    def setUp(self):
+        self.data_prep_left = TwitterDataProcessor(orient='left')
+        self.data_prep_right = TwitterDataProcessor(orient='right')
+        self.data_prep_frac_left = TwitterDataProcessor(orient='left', frac_data=True,
+                                                        frac_start=0.0, frac_end=0.5)
+
+    def tearDown(self):
+        pass
+
+    def test_load_raw_data(self):
+        # Tests that file exists:
+        self.assertTrue(os.path.exists(self.data_prep_left.users_file_path))
+
+    def test_get_retweet_data(self):
+        # Test preprocessing step for left-wing participants/full data:
+        self.data_prep_left.load_raw_data()
+        self.data_prep_left.preprocess_data()
+
+        # Define left-wing user dataframe:
+        left_users_df = self.data_prep_left.users_df
+
+        # Assert that all user ratings were converted to positive scale:
+        self.assertTrue(all(left_users_df['orig_rating'] > 0))
+
+        # Test preprocessing step for right-wing/full data:
+        self.data_prep_right.load_raw_data()
+        self.data_prep_right.preprocess_data()
+
+        # Define right-wing user dataframe and assert all positive ratings:
+        right_users_df = self.data_prep_left.users_df
+        self.assertTrue(all(right_users_df['orig_rating'] > 0))
+
+        # Preprocess fraction of left-wing users dataframe:
+        self.data_prep_frac_left.load_raw_data()
+        self.data_prep_frac_left.preprocess_data()
+
+        # Define retweet dataframe for fraction of left-wing users:
+        frac_left_rt_df = self.data_prep_frac_left.rt_df
+
+        # Get number of unique users in left-wing fraction dataframe:
+        n_users_frac_left = len(np.unique(frac_left_rt_df['userid']))
+
+        # Define fraction end of left-wing fraction dataframe:
+        frac_left_end = self.data_prep_frac_left.frac_end
+
+        # Get unique number of users in full left-wing retweet dataframe:
+        left_rt_df = self.data_prep_left.rt_df
+        n_users_left = len(np.unique(left_rt_df['userid']))
+
+        # Assert that proper fraction of users represented in fraction dataframe:
+        self.assertTrue(n_users_frac_left == int(frac_left_end * n_users_left))
+
+        # Join left-wing dataframes:
+        self.data_prep_left.join_data()
+
+        # Assert that a non-zero length dataframe exists:
+        self.assertTrue(len(self.data_prep_left.rt_df) > 0)
+
+        # Assert non-zero length dataframe exists for fraction dataframe:
+        self.data_prep_frac_left.join_data()
+        self.assertTrue(len(self.data_prep_frac_left.rt_df) > 0)
+
+if __name__ == '__main__':
+    unittest.main()
